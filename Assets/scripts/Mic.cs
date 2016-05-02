b29604478261ebebd1a8c206a89a68e1c306da13
aquira246 <aacost06@calpoly.edu>
2016-05-02 15:45:57 -0700

    Added in stuff to echo microphone, just needs some modification

---
+++ Assets/scripts/Mic.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mic : MonoBehaviour {
+
+    public AudioClip c;
+    public string MicName;
+
+    void Start()
+    {
+        if (Microphone.devices.Length > 0)
+        {
+            MicName = Microphone.devices[0];
+        }
+        else
+        {
+            MicName = "Microphone";
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (Microphone.IsRecording(MicName))
+            {
+                StopRecording();
+            }
+            else
+            {
+                StartRecording();
+            }
+        }
+	}
+
+    void StopRecording()
+    {
+        Microphone.End(MicName);
+        GetComponent<AudioSource>().Stop();
+    }
+
+    void StartRecording()
+    {
+        c = Microphone.Start(MicName, true, 300, 48000); // the last one is frequency, that changes with device
+        while (!(Microphone.GetPosition(MicName) > 0)) 
+        {
+            GetComponent<AudioSource>().PlayOneShot(c);
+        }
+    }
+}
